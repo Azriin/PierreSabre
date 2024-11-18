463f085af8b5b9ae56de23dfef350fb7f2a1881c
Antoni Metge <mtn5047a@ensfsi.onmicrosoft.com>
2024-11-18 16:39:01 +0100

    partie yaku tp5

--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -39,4 +39,10 @@ public class Yakuza extends Humain{
 		gagnerArgent(gain);
 		parler("Ce ronin pensait vraiment battre " + getNom() + " du clan de " + clan + "? Je l'ai dépouillé de ses "+ gain +" sous.");
 	}
+	
+	@Override
+	public void direBonjour() {
+		super.direBonjour();
+		parler("Mon clan est celui de " + clan);
+	}
 }
